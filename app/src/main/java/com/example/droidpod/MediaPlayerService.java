56c2494a568207ec8816f059e27c0ac1e6d447ee
CJ Olsen <cjols97@iastate.edu>
2021-03-02 21:16:31 -0600

    Audio manager, error handling, and audio focus handling

--- app/src/main/java/com/example/droidpod/MediaPlayerService.java
+++ app/src/main/java/com/example/droidpod/MediaPlayerService.java
@@ -1,12 +1,14 @@
 package com.example.droidpod;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
 import java.io.IOException;
 
@@ -21,6 +23,7 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
 
     private MediaPlayer mediaPlayer;
     private String mediaFile;
+    private AudioManager audioManager;
     private int resumePosition;
 
     /**
@@ -94,48 +97,146 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
     // Binder given to clients
     private final IBinder iBinder = new LocalBinder();
 
+    /**
+     *
+     * @param intent
+     * @return
+     */
     @Override
     public IBinder onBind(Intent intent) {
         return iBinder;
     }
 
+    /**
+     * Invoked when the network stream has updated buffering
+     * @param mp
+     * @param percent
+     */
     @Override
     public void onBufferingUpdate(MediaPlayer mp, int percent) {
         //Invoked indicating buffering status of
         //a media resource being streamed over the network.
     }
 
+    /**
+     * Stops media and service
+     * Invoked upon completion of media playback
+     * @param mp
+     */
     @Override
     public void onCompletion(MediaPlayer mp) {
-        //Invoked when playback of a media source has completed.
+        stopMedia();
+        stopSelf();
     }
 
-    //Handle errors
+    /**
+     * Error handling function
+     * Catches asynchronous operational errors
+     * @param mp MediaPlayer object
+     * @param i error
+     * @param extra error descriptor
+     * @return false on completion
+     */
     @Override
-    public boolean onError(MediaPlayer mp, int what, int extra) {
-        //Invoked when there has been an error during an asynchronous operation.
+    public boolean onError(MediaPlayer mp, int i, int extra) {
+        switch (i) {
+            case MediaPlayer.MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK:
+                Log.d("MediaPlayer Error", "MEDIA ERROR NOT VALID FOR PROGRESSIVE PLAYBACK " + extra);
+                break;
+            case MediaPlayer.MEDIA_ERROR_SERVER_DIED:
+                Log.d("MediaPlayer Error", "MEDIA ERROR SERVER DIED " + extra);
+                break;
+            case MediaPlayer.MEDIA_ERROR_UNKNOWN:
+                Log.d("MediaPlayer Error", "MEDIA ERROR UNKNOWN " + extra);
+                break;
+        }
         return false;
     }
 
+    /**
+     * Invoked when info is being communicated
+     * @param mp
+     * @param what
+     * @param extra
+     * @return
+     */
     @Override
     public boolean onInfo(MediaPlayer mp, int what, int extra) {
-        //Invoked to communicate some info.
+
         return false;
     }
 
+    /**
+     * Starts media playback
+     * Invoked when the media playback is primed.
+     * @param mp
+     */
     @Override
     public void onPrepared(MediaPlayer mp) {
-        //Invoked when the media source is ready for playback.
+        playMedia();
     }
 
+    /**
+     * Invoked when the seek call has completed
+     * @param mp
+     */
     @Override
     public void onSeekComplete(MediaPlayer mp) {
-        //Invoked indicating the completion of a seek operation.
+
     }
 
+    /**
+     * Invoked when the audio focus changes
+     * @param focusChange
+     */
     @Override
     public void onAudioFocusChange(int focusChange) {
-        //Invoked when the audio focus of the system is updated.
+        switch (focusChange) {
+            // audio focus gain => resume playback
+            case AudioManager.AUDIOFOCUS_GAIN:
+                if (mediaPlayer == null)
+                    initMediaPlayer();
+                else if (!mediaPlayer.isPlaying())
+                    mediaPlayer.start();
+                mediaPlayer.setVolume(1.0f, 1.0f);
+                break;
+            // audio focus loss => stop and release player
+            case AudioManager.AUDIOFOCUS_LOSS:
+                if (mediaPlayer.isPlaying())
+                    mediaPlayer.stop();
+                mediaPlayer.release();
+                mediaPlayer = null;
+                break;
+            // short audio focus loss => pause playback
+            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                if (mediaPlayer.isPlaying())
+                    mediaPlayer.pause();
+                break;
+            // short audio focus loss (e.g. notification) => lower volume
+            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+                if (mediaPlayer.isPlaying())
+                    mediaPlayer.setVolume(0.1f, 0.1f);
+                break;
+        }
+    }
+
+    /**
+     * Requests audio focus
+     * @return true if request has been granted
+     */
+    private boolean requestAudioFocus() {
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int result = audioManager.requestAudioFocus(
+                this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+        return result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED;
+    }
+
+    /**
+     * Removes audio focus
+     * @return true if request to remove audio has been granted
+     */
+    private boolean removeAudioFocus() {
+        return AudioManager.AUDIOFOCUS_REQUEST_GRANTED == audioManager.abandonAudioFocus(this);
     }
 
     public class LocalBinder extends Binder {
